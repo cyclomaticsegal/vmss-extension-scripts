7cd7b7934c9ecb2d99e9ceaf97b6cedd867a00ff
Simon Segal <utzbach@gmail.com>
2019-09-10 08:03:19 +1000

    Update DisableAutoUpdateVmss.ps1
    remove 2012 lines

--- DisableAutoUpdateVmss.ps1
+++ DisableAutoUpdateVmss.ps1
@@ -24,13 +24,6 @@
 # 12/13/2018 - Added descriptions.
 ######################################################################################################################################################
 
-#Only works up to Win2012.
-#https://docs.microsoft.com/en-us/windows/desktop/api/wuapi/ne-wuapi-tagautomaticupdatesnotificationlevel
-#http://www.darrylvanderpeijl.com/windows-server-2016-update-settings/
-#$AUSettings = (New-Object -com "Microsoft.Update.AutoUpdate").Settings 
-#$AUSettings.NotificationLevel = 1
-#$AUSettings.Save
-
 #For Win2016 and above.
 #https://4sysops.com/archives/disable-windows-10-update-in-the-registry-and-with-powershell/
 New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name WindowsUpdate -Force
